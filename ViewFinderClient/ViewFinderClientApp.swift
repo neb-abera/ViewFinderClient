255092837de6cc0ac49c40845173e499c121a06f
Nebyou Abera <nebyouabera@gmail.com>
2023-09-03 00:47:15 -0500

    Initial Commit

---
+++ ViewFinderClient/ViewFinderClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewFinderClientApp.swift
+//  ViewFinderClient
+//
+//  Created by Nebyou Abera on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewFinderClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
